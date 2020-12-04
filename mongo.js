f998d88d5022334cef89ff24b6750aaaecea8134
subodh mishra <subodh@shipgigventures.com>
2020-12-04 17:35:32 +0530

    changes in mongo.js file

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-var mongodbErrorHandler = require("mongoose-mongodb-errors");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// var mongodbErrorHandler = require("mongoose-mongodb-errors");
 
-mongoose.Promise = global.Promise;
-mongoose.plugin(mongodbErrorHandler);
-mongoose.connect(process.env.MONGO_URI);
+// mongoose.Promise = global.Promise;
+// mongoose.plugin(mongodbErrorHandler);
+// mongoose.connect(process.env.MONGO_URI);
  
